783458f89c0cbb901da87d1ffe7e27f79c0a6d15
Caoshenyang <2417254000@qq.com>
2025-03-24 15:13:52 +0800

    feat: 整合tailwindcss4.x

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
 <template>
 
   <el-button type="primary" size="default">登录</el-button>
+  <h1 class="text-center text-2xl text-red-500">
+    Hello world!
+  </h1>
 </template>
 
 <style scoped></style>
