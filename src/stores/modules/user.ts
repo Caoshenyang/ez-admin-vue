53de0518b6773679774bc5be90cbada401c73c6b
Caoshenyang <2417254000@qq.com>
2025-06-03 17:42:23 +0800

    feat: 整合pinia并引入持久化插件

---
+++ src/stores/modules/user.ts
@@ -0,0 +1,84 @@
+import { defineStore } from 'pinia'
+
+// 你可以对 `defineStore()` 的返回值进行任意命名，但最好使用 store 的名字，同时以 `use` 开头且以 `Store` 结尾。(比如 `useUserStore`，`useCartStore`，`useProductStore`)
+// 第一个参数是你的应用中 Store 的唯一 ID。
+export const useUserStore = defineStore('userInfo', {
+  state: () => ({
+    userInfo: {} as UserInfoVO,
+    workTabList: [{ title: '工作台', path: HOME_PAGE }] as WorkTab[],
+    buttonRecords: {} as Record<string, ActionItem[]>,
+  }),
+  getters: {
+    avatar: (state) => {
+      return state.userInfo.avatar || '/images/avatar_boy.jpeg'
+    },
+    hasPermission: (state) => (value: string | string[]) => {
+      if (typeof value === 'string') {
+        return state.userInfo.permissions.includes(value)
+      }
+      return value.some((permission) => state.userInfo.permissions.includes(permission))
+    },
+  },
+  actions: {
+    /** 是否登录 */
+    isLogin() {
+      return !!getToken()
+    },
+
+    /**
+     * 登录
+     *
+     * @param loginDTO 登录参数
+     */
+    async login(loginDTO: LoginDTO) {
+      const token = await authApi.login(loginDTO)
+      console.log(token)
+
+      setToken(token)
+    },
+
+    /** 获取用户信息 */
+    async getUserInfo() {
+      try {
+        const userInfo = await authApi.getUserInfo()
+        this.userInfo = userInfo
+      } catch (error) {
+        this.logout()
+        console.log(error)
+      }
+    },
+    // 退出登录
+    async logout() {
+      // 请求后端接口
+      // await logout()
+
+      // 清除数据和状态
+      removeToken()
+      this.$reset()
+    },
+    // 获取当前路由的按钮
+    getButtons(routePath: string): ActionItem[] {
+      return this.buttonRecords[routePath] || []
+    },
+    // 收集按钮并存入缓存
+    cacheButtons(fullPath: string, menu: MenuTreeVO) {
+      if (menu.menuType === MenuType.BUTTON) return
+      // 存入当前路由的按钮
+      if (menu.children?.length) {
+        const buttons = this.collectButton(menu.children)
+        if (buttons.length) {
+          this.buttonRecords[fullPath] = buttons
+        }
+      }
+    },
+    // 辅助方法：收集按钮
+    collectButton(menuList: MenuTreeVO[]): ActionItem[] {
+      return menuList.flatMap((item) =>
+        item.menuType === MenuType.BUTTON
+          ? [useMenuTreeToActionItem(item)]
+          : this.collectButton(item.children || []),
+      )
+    },
+  },
+  persist: true,
+})
---
+++ src/types/user.d.ts
@@ -0,0 +1,8 @@
+// 用户信息
+export interface UserInfoVO {
+  userId: string
+  username: string
+  nickname: string
+  avatar: string
+  menus: MenuTreeVO[]
+}
